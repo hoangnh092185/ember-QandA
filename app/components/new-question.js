4d1e0a2a53f197665a48483786dc61d77bf8166d
Nhat Hoang <hoangnh092185@gmail.com>
2016-10-28 11:18:36 -0700

    create add new question functionality to the index page.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow(){
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion1(){
+      var params = {
+        name: this.get('name'),
+        title: this.get('title'),
+        content: this.get('content')
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
 });
