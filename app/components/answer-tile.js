5a5254606548e36821245fe67ac88d8de7866661
Nhat Hoang <hoangnh092185@gmail.com>
2016-11-04 10:35:05 -0700

    add rating functionality.

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('answer.name', 'answer.rating', function(){
+    return this.get('answer.name') + ' - ' + this.get('answer.rating')
+  }),
   actions: {
     delete(answer) {
       if (confirm('Are you sure you want to delete this answer?')) {
