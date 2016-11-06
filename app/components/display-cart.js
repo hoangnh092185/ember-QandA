8db4141246d45f3f8a8a31b026b28cd842b98de4
Nhat Hoang <hoangnh092185@gmail.com>
2016-11-06 14:17:47 -0800

    add service question cart.

---
+++ app/components/display-cart.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // questionCart: Ember.inject.service(),        
+  // actions: {
+  //   remove(question){
+  //     this.get('questionCart').remove(question);
+  //   }
+  // }
+});
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // questionCart: Ember.inject.service(),
+  //
+  // actions: {
+  //   add(question){
+  //     this.get('questionCart').add(question);
+  //   }
+  // }
 });
