dc5ac3baa68d3b1e883b9d31cc5d953f2a0d0d2f
Nhat Hoang <hoangnh092185@gmail.com>
2016-11-06 11:58:36 -0800

    add helper to display star when question have more certain answer.

--- app/helpers/answer-popularity.js
+++ app/helpers/answer-popularity.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 export function answerPopularity(params) {
   var question = params[0];
-  console.log(question.get('answer').get('length'))
-  if (question.get('answer').get('length') >= 2){
-    return Ember.String.htmlSafe('<span class="glyphico glyphicon-fire"></span>');
+  console.log(question.get('answer').get('length'));
+  if (question.get('answer').get('length') >= 5){
+    return Ember.String.htmlSafe('<h2><span class="glyphicon glyphicon-star"></span></h2>');
+    console.log("read this");
   }
-  return params;
+  return ;
 }
 export default Ember.Helper.helper(answerPopularity);
