ed3df6afac8b319279bbb9623fbd4fdbd7a7093b
Nhat Hoang <hoangnh092185@gmail.com>
2016-11-08 07:45:27 -0800

    install npm install moment to fix ember-cli-moment node error.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,16 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  model(questionIndex){
+  // model(){
+  //   return Ember.RSVP.hash({
+  //     questions: this.store.findAll('question'),
+  //     answers: this.store.findAll('answer')
+  //   });
+  // },
+  // model(questionIndex){
     // return this.store.findRecord('question', params.question_id);
-    return this.store.findAll('question');
-  },
+  //   return this.store.findAll('question');
+  // },
   // questionIndex: this.store.findAll('question'),
 
   questionCart: Ember.inject.service(),
-
   canAddToList: Ember.computed('questionCart.questions.[]', function(){
-       return this.get('questionCart').includes(questionIndex);
+      var questionIndex = this.store.findALl('question');
+      console.log(questionIndex);
+      return this.get('questionCart').includes(questionIndex);
      }),
 
   actions: {
