0effa41eadcc885e5cfa2ab4040630c28700e01d
nhat Hoang <hoangnh092185@gmail.com>
2016-11-09 22:17:40 -0800

    fix ID errors in update question file.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+
   questionCart: Ember.inject.service(),
+  // 
+  // canAddTolist: Ember.computed('questionCart.questions', function(){
+  //
+  //   return this.get('questionCart.questions').includes(this);
+  // }),
+
   actions: {
     addToList(question){
         this.get('questionCart').add(question);
--- app/services/question-cart.js
+++ app/services/question-cart.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
     questions: [],
-    resetButton: false,
+    
     add(question) {
       if(this.get('questions').includes(question)){
         alert('Question already added to the list');
@@ -14,7 +14,6 @@ export default Ember.Service.extend({
       this.get('questions').removeObject(question);
     },
     empty() {
-      this.set('resetButton', true);
       this.get('questions').setObjects([]);
     },
     includes(question){
